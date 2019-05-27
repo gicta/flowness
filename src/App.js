666f2321e1133cbca272d92cd7d2d5771b66bb91
gicta <code@gicta.com>
2019-05-27 11:40:22 -0400

    added feature001

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Eat More React!  Feature 001
         </a>
       </header>
     </div>
